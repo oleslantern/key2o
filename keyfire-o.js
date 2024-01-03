d241d527d8ec282a51e51d6326bf5134ac2ba8b2
oleslantern <139644842+oleslantern@users.noreply.github.com>
2024-01-03 11:13:55 -0500

    Update keyfire-o.js

--- keyfire-o.js
+++ keyfire-o.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Google Userscript
+// @name         Google v2 Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
